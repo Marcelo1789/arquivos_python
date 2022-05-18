9c334581b1544a1f673c33f65da3d1424683b699
Marcelo <marcelo1789@hotmail.com>
2022-05-18 18:15:59 -0300

    continue

---
+++ fatura.py
@@ -0,0 +1,8 @@
+compras = input ('digite o nome do produto: ')
+produto=[]
+produto.append (compras)
+x = 0
+
+while x < 5:
+    x = x+1
+    print (produto)
---
+++ loop_for.py
@@ -0,0 +1,20 @@
+## loop for com listas e string 
+
+compras = ['arroz','macarrão','carne','feijão']
+
+for x in compras: 
+    print (x)
+
+nome = 'Marcelo'
+for x in nome:
+    print (x)
+
+## somatoria com loop for 
+vendas  = [1000,450,920,600]
+total = 0
+for x in vendas:
+    total = total + x
+    print (total) 
+
+for i in range (0,10):
+    print (10 - i)
