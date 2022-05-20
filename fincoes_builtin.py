7788de353f1b91828ecf85dd7f1e46f6f2487720
Marcelo <marcelo1789@hotmail.com>
2022-05-20 17:11:56 -0300

    bult-in

---
+++ fincoes_builtin.py
@@ -0,0 +1,4 @@
+import math 
+vendas = [230,240,567]
+marcelo = math.fsum (vendas)
+print (marcelo)
--- função reloaded.py
+++ função reloaded.py
@@ -1,3 +1,9 @@
 def mensagem():
     print ('Hello World')
 mensagem()
+
+def flor (cor = 'vermelho', flor = 'rosa'):
+    print ("a cor da flor é ", cor , "E a flor é ", flor)
+flor()
+flor ('azul', 'violeta') 
+flor ('verde','folha')
